c61ceb9b6a4340038bb4684a6a332765b4f3bf85
shiyivei <shiyivei@outlook.com>
2022-10-20 00:47:39 +0800

    cache images

--- rust-courses/rust-course-chentian/thumbor-05/src/main.rs
+++ rust-courses/rust-course-chentian/thumbor-05/src/main.rs
@@ -1,4 +1,121 @@
+use axum::{
+    extract::{Extension, Path},
+    http::{HeaderMap, HeaderValue, StatusCode},
+    routing::get,
+    Router,
+};
+
+use percent_encoding::{percent_decode_str, percent_encode, NON_ALPHANUMERIC};
+use serde::Deserialize;
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    sync::Arc,
+};
 mod pb;
-fn main() {
+use anyhow::Result;
+use bytes::Bytes;
+use lru::LruCache;
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tracing::{info, instrument};
+
+use pb::*;
+use tower_http::add_extension::AddExtensionLayer;
+
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    // 初始化tracing
+    tracing_subscriber::fmt::init();
+
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(1024)));
+
+    //构建路由
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(cache))
+                .into_inner(),
+        );
+    //运行web服务器
+    let addr = "127.0.0.1:3000".parse().unwrap();
+
+    print_test_url("https://imgaes.pexels.com/photos/1562477/pexels/-photo-1562477");
+
+    info!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
     println!("Hello, world!");
+}
+
+// 解析参数
+async fn generate(
+    Path(Params { spec, url }): Path<Params>,
+    Extension(cache): Extension<Cache>,
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    let url: &str = &percent_decode_str(&url).decode_utf8_lossy();
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    let mut headers = HeaderMap::new();
+
+    headers.insert("content-type", HeaderValue::from_static("image/jpeg"));
+
+    Ok((headers, data.to_vec()))
+}
+
+#[instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+
+    let g = &mut cache.lock().await;
+    let data = match g.get(&key) {
+        Some(v) => {
+            info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            info!("Retrive url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+
+            g.put(key, data.clone());
+            data
+        }
+    };
+    Ok(data)
+}
+
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let sepc1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let sepc2 = Spec::new_watermark(20, 20);
+    let sepc3 = Spec::new_filter(filter::Filter::Marine);
+
+    let image_spec = ImageSpec::new(vec![sepc1, sepc2, sepc3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    println!("test url: http://localhost:3000/image/{}/{}", s, test_image);
 }
--- rust-notes-chentian.md
+++ rust-notes-chentian.md
@@ -50,3 +50,9 @@ reqwest = {version = "0.11", features = ["json"]}
 tokio = {version = "1", features =["full"]} 
 ```
 
+# 5 做一个图片服务器
+
+```
+httpie get "http://localhost:3000/image/CgoKCAjYBBCgBiADCgY6BAgUEBQKBDICCAM/https%3A%2F%2Fimages%2Epexels%2Ecom%2Fphotos%2F2470905%2Fpexels%2Dphoto%2D2470905%2Ejpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D750%26w%3D1260"
+```
+
